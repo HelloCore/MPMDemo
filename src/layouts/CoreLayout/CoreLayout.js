3f04ae76c42be2c9f0b197bb097c89de3e7cdc79
AKKHARAWAT CHAYAPIWAT <aukwat@hotmail.com>
2017-03-04 23:20:47 +0700

    Add LoadingView to CoreLayout

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
 import Header from '../../components/Header'
+import Spinner from 'react-spinkit'
+
 import './CoreLayout.scss'
 import '../../styles/core.scss'
 
-export const CoreLayout = ({ children }) => (
-  <div className='mainContainer'>
-    <Header />
-    <div className='container text-center'>
-      <div className='core-layout__viewport'>
-        {children}
+class CoreLayout extends Component {
+  render(){
+    return (
+      <div className='mainContainer'>
+        <Header />
+        <div className='container text-center'>
+          <div className='core-layout__viewport'>
+            {this.props.children}
+          </div>
+        </div>
+        {this.props.isLoading &&
+          <div className='core-layout__global-loading-container'>
+            <div className='core-layout__global-loading'>
+              <Spinner spinnerName="three-bounce" />
+            </div>
+          </div>
+        }
       </div>
-    </div>
-  </div>
-)
-
-CoreLayout.propTypes = {
-  children : React.PropTypes.element.isRequired
+    )
+  }
 }
 
-export default CoreLayout
+CoreLayout.propTypes = {
+  children : PropTypes.element.isRequired
+}
+
+const mapDispatchToProps = {
+
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoading: (state.user.isLoading || state.timesheet.isLoading),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoreLayout)
